7f40d2f0d0f61511428a1a78e3fbbf77ee9fe049
MikeForde <mikeforde@ymail.com>
2024-07-15 12:04:56 +0000

    IPSModel to env

--- models/IPSModel.js
+++ models/IPSModel.js
@@ -1,6 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('test', 'root', 'password', {
-    host: '172.17.0.2',
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
