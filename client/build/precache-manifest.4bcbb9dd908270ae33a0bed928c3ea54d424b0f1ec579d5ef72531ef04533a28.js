aa1569dca4dd70458ab96997c7e08afe8214e8a7
MikeForde <mikeforde@ymail.com>
2024-09-04 13:46:48 +0000

    build trial

---
+++ client/build/precache-manifest.4bcbb9dd908270ae33a0bed928c3ea54d424b0f1ec579d5ef72531ef04533a28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556c75e939832710b55106d22acdec828e31e3e26db32c6c88adf1c4cafd51fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c868c415b4b742005b",
+    "url": "/static/css/2.c6f8e76a.chunk.css"
+  },
+  {
+    "revision": "bd0becf06afc075000d0",
+    "url": "/static/css/main.b0bcc89a.chunk.css"
+  },
+  {
+    "revision": "b4c868c415b4b742005b",
+    "url": "/static/js/2.9f8fc012.chunk.js"
+  },
+  {
+    "revision": "170a67b9fba0b8f18fb6cbf785320179b0cdd568f42bba55c924fc3a2f3de8ab",
+    "url": "/static/js/2.9f8fc012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0becf06afc075000d0",
+    "url": "/static/js/main.f71f8035.chunk.js"
+  },
+  {
+    "revision": "c8d71d31bae30f758dc7",
+    "url": "/static/js/runtime-main.03efc685.js"
+  }
+]);
