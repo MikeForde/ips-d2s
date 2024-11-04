b05f8530c4a1f32358974de750dfe3bf7c4a0935
MikeForde <mikeforde@ymail.com>
2024-11-04 10:49:14 +0000

    For demo

--- servercontrollers/postIPSBundle.js
+++ servercontrollers/postIPSBundle.js
@@ -5,6 +5,7 @@ async function postIPSBundle(req, res) {
 
   try {
     const response = await axios.post('https://4202xiwc.offroadapps.dev:62444/Fhir/ips/json', ipsBundle);
+    //const response = await axios.post('https://ips-d2s-uksc-medsnomed-medsno.apps.ocp1.azure.dso.digital.mod.uk/ipsbundle/', ipsBundle);
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
