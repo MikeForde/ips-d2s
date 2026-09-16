03d5770a617d05f396f963310e988af3232a8961
MikeForde <mikeforde@ymail.com>
2026-09-16 15:06:10 +0000

    Deployment 0_86l - npm audit fixes

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { PatientProvider } from './PatientContext';
-import 'react-native-get-random-values';
 
 
 ReactDOM.render(
